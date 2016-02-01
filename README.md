8eab4b36c01ea08f6547dc2575b945d3788b86da
Andrew Aslinger <aaslinger@openwhere.com>
2016-02-01 09:04:23 -0500

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ecs-deploy
-Automate Deployment of ECS Tasks and Services
+Automate Deployment of AWS ECS (Elastic Container Service) Tasks and Services
+
+This is a simple python script which allows you to update the deployment of new versions of ECS Tasks and Services. This script assumes you have another build process releasing the new containers. This script then allows you to automatically bump your Task or Service to the new Docker tag or version.
+
+## Usage
+`pip install boto3`
+`python deploy.py --help`
+
+## Example
+Bump a task version `deploy.py --cluster your-cluster --region us-east-1 --task task-name`
+Bump a service version `deploy.py --cluster your-cluster --region us-east-1 --service service-name`
+
+##AWS Credentials
+This script assumes you have exposed your AWS credentials by one of the typical means, env variables, ~/.aws/credentials, or an IAM Role.
+https://github.com/boto/boto3
+
+
+##Credits
+This script was inspired by https://github.com/silinternational/ecs-deploy. It was created independently as we needed to manage the deployment of tasks and not just services and prefer to work in python for maintainability of our DevOps automation vs bash. 
---
+++ deploy.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import argparse, boto3
+
+class ApplyECS():
+
+    def main(self, cluster, region, service, name):
+
+        self.load(cluster, region, service, name)
+
+    def load(self, cluster, region, service, name):
+
+        client = boto3.client('ecs', region_name=region)
+
+        if service is not None:
+           result = client.describe_services(
+                cluster=cluster,
+                services=[
+                    service,
+                ]
+            )
+           if len(result['services']) == 0:
+               print "No service found " + ' '.join(map(lambda f: f['reason'] + ' ' + f['arn'], result['failures']))
+               exit(1)
+           else:
+               service1 = result['services'][0]
+               taskdef = service1['taskDefinition']
+
+        elif name is not None:
+            result = client.list_task_definitions(
+                            familyPrefix=name,
+                            sort='DESC',
+                            maxResults=1
+                        )
+            if len(result['taskDefinitionArns']) == 0:
+               print "No service found " + ' '.join(map(lambda f: f['reason'] + ' ' + f['arn'], result['failures']))
+               exit(1)
+            else:
+                taskdef = result['taskDefinitionArns'][0]
+
+        print "Got task def: " + taskdef
+
+        response = client.describe_task_definition(
+                        taskDefinition=taskdef
+                    )
+
+        ##Todo - semantic versioning
+
+        new_def = {
+            'family': response['taskDefinition']['family'],
+            'volumes': response['taskDefinition']['volumes'],
+            'containerDefinitions': response['taskDefinition']['containerDefinitions'],
+        }
+        print new_def
+
+        #Register Task Definition
+        response = client.register_task_definition(**new_def)
+        status =  response['ResponseMetadata']['HTTPStatusCode']
+        if status == 200:
+            print "Result of Update: " + response['taskDefinition']['taskDefinitionArn']
+            updated_task = response['taskDefinition']['taskDefinitionArn']
+        else:
+            print "Error registering " + taskdef
+            exit(1)
+
+        if service is not None:
+            print "Updating Service"
+            response = client.update_service(
+                cluster=cluster,
+                taskDefinition=updated_task,
+                service=service
+            )
+            status = response['ResponseMetadata']['HTTPStatusCode']
+            if status == 200:
+                print "Result of service update: " + response['service']['serviceArn']
+            else:
+                print "Error updating service " + service
+                exit(1)
+
+            ##Todo - monitor service to see if it comes up
+
+if __name__ == '__main__':
+    p = ApplyECS()
+
+    parser = argparse.ArgumentParser(description='Download JSON from a Rest Endpoint to files. Slashes in id converted to __')
+    parser.add_argument('--cluster', help='name of the ecs cluster', default='default')
+    parser.add_argument('--region', help='region in aws', default='us-east-1')
+    parser.add_argument('--service', help='Name of the Service')
+    parser.add_argument('--task', help='name of task')
+    args = parser.parse_args()
+
+    if(args.service is None and args.task is None):
+        print "ERROR - Either a service or task name is required"
+        exit(1)
+
+    p.main(args.cluster, args.region, args.service, args.task)
