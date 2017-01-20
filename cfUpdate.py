aa41d408750424d3cb6745e57b461d70052dc9d5
owms <msanders@openwhere.com>
2017-01-20 08:24:57 -0700

    Updated comment to reflect AWS CF API behavior.

--- cfUpdate.py
+++ cfUpdate.py
@@ -108,7 +108,7 @@ class ApplyCF:
     def find_existing_stack(self, cf_client, cf_params, service_name):
         existing_stack_id = None
         try:
-            # There should be only 1 stack returned if running because we are using the stack name instead of ID/ARN
+            # There will be at most 1 stack returned if running because we are using the stack name instead of ID/ARN
             existing_stacks = cf_client.describe_stacks(StackName=service_name)
             if existing_stacks is not None and len(existing_stacks) > 0:
                 existing_stack_id = existing_stacks['Stacks'][0]['StackId']
