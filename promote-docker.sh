793741f7116d0e5158ebc78f685620d895f5a823
owms <msanders@openwhere.com>
2017-01-31 12:30:20 -0700

    Make repo name a variable.

--- promote-docker.sh
+++ promote-docker.sh
@@ -7,13 +7,14 @@
 set -e
 
 IMAGE_NAME=ecs-deploy
-TAG=${1:-latest}
+DOCKER_REPO=${1}
+TAG=${2:-latest}
 
 DOCKER_VERSION=`docker --version | cut -f3 | cut -d '.' -f2`
 [ ${DOCKER_VERSION} -lt 12 ] && TAG_FLAG='-f' || TAG_FLAG=''
 
 $(aws ecr get-login)
-docker tag ${TAG_FLAG} 639193537090.dkr.ecr.us-east-1.amazonaws.com/develop/${IMAGE_NAME}:${TAG} 639193537090.dkr.ecr.us-east-1.amazonaws.com/release/${IMAGE_NAME}:${TAG}
-docker push 639193537090.dkr.ecr.us-east-1.amazonaws.com/release/${IMAGE_NAME}:${TAG}
+docker tag ${TAG_FLAG} ${DOCKER_REPO}/develop/${IMAGE_NAME}:${TAG} ${DOCKER_REPO}/release/${IMAGE_NAME}:${TAG}
+docker push ${DOCKER_REPO}/release/${IMAGE_NAME}:${TAG}
 
 
